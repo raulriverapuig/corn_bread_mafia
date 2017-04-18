45210a3acad7ac488e0abe5c79b993fbfbe3a2c3
Julie Collings <juliecollings@mail.weber.edu>
2017-04-18 15:01:01 +0000

    Initial commit

---
+++ hw10_task2.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hw10_task2.c
+ *       	Usage:  ./hw10_task2.c
+ *    Description:  `
+ *
+ *        Version:  1.0
+ *        Created:  04/18/2017 02:59:32 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Julie Collings (), juliecollings@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Definitions */
+
+
